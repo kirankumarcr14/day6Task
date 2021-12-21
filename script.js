cf963e8cb16e5df5a7e368abed5847649e91aa12
KIRAN KUMAR G <84234017+kirankumarcr14@users.noreply.github.com>
2021-12-21 15:09:49 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+ 
+ alert("I`m invoked!");
+
+ let admin=9, fname=10.5;
+ fname="Guvi";
+ lname="geek";
+ admin=fname+ " " + lname;
+ alert(admin);
+
+ let name = 10.5;
+ first = "Guvi";
+ last = " geek";
+  let fullname = first +last ;
+   alert (fullname);
+
+   let a=prompt("First number?");
+   let b=prompt("Second number?");
+
+   alert(+a + +b);
+
+   
